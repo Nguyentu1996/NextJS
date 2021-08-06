555dea452099df44a3f9519661fcc3df2c62575d
nguyentu1996 <nguyentu19962710@gmail.com>
2021-08-06 15:37:12 +0700

    ok

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const apiUrl = process.env.NODE_ENV === 'development' 
-    ? 'http://thuongmainongsan.com:8080' // development api
-    : 'http://thuongmainongsan.com:8080'; // production api
+    ? 'http://thuongmainongsan.com:8080/thuongmainongsan' // development api
+    : 'http://thuongmainongsan.com:8080/thuongmainongsan'; // production api
 const access = {
     myResourceDir: 'C:/web/TmdtWebResource/tmdt',
 }
-const domain = "http://thuongmainongsan.com:8080/"
+const domain = "http://thuongmainongsan.com:8080/thuongmainongsan/"
 export {
     apiUrl,
     access,
