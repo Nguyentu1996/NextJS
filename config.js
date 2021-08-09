8210f501cdf2cef7fdc7d18e89f0e61f98cc4016
nguyentu1996 <nguyentu19962710@gmail.com>
2021-08-09 15:57:42 +0700

    update config

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const apiUrl = process.env.NODE_ENV === 'development' 
-    ? 'https://qrcodehaugiang.com.vn:8080/thuongmainongsan/' // development api
-    : 'https://qrcodehaugiang.com.vn:8080/thuongmainongsan/'; // production api
+    ? 'https://qrcodehaugiang.com.vn:8080/thuongmainongsan' // development api
+    : 'https://qrcodehaugiang.com.vn:8080/thuongmainongsan'; // production api
 const access = {
     myResourceDir: 'C:/web/TmdtWebResource/tmdt',
 }
-const domain = "https://qrcodehaugiang.com.vn:8080/thuongmainongsan/"
+const domain = "https://qrcodehaugiang.com.vn:8080/thuongmainongsan"
 export {
     apiUrl,
     access,
